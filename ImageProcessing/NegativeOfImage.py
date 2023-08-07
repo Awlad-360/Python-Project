454f3f4c2c3496e0421f8edcb89493cca6c72b5c
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-08 00:14:45 +0600

    NegativeOfImage.py
    Complete

--- ImageProcessing/NegativeOfImage.py
+++ ImageProcessing/NegativeOfImage.py
@@ -1,1 +1,8 @@
-NegativeOf image
+import cv2
+from google.colab.patches import cv2_imshow
+img = cv2.imread('/content/ainstein.jpg')
+negative_image = abs(255-img)
+cv2_imshow(img)
+cv2_imshow(negative_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
