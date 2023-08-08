3aa8a15cea210779bafd2d2b887b41183bc9bac5
Awlad Hossain <140268506+Sp-Awlad-360@users.noreply.github.com>
2023-08-08 07:48:46 +0600

    BoundaryExtraction.py
    Complete

--- ImageProcessing/BoundaryExtraction.py
+++ ImageProcessing/BoundaryExtraction.py
@@ -1,1 +1,11 @@
-Boundary extraction
+import cv2
+import numpy as np
+from google.colab.patches import cv2_imshow
+img = cv2.imread('mango.jpg')
+kernel = np.ones((5, 5), np.uint8)
+img_erosion = cv2.erode(img, kernel, iterations=1)
+boundary_extraction=img-(img_erosion)
+cv2_imshow(img)
+cv2_imshow(boundary_extraction)
+cv2.waitKey(0)
+
